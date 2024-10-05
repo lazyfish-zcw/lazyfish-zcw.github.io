941d156646e326883a2e15174a38f0b780499a77
MKCODER\milkp <1993985434@qq.com>
2024-10-05 20:39:47 +0800

    Site updated: 2024-10-05 20:39:41

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/25/hello-world/","2024/09/26/简单文件压缩器/"];function toRandomPost(){
+var posts=["2024/09/25/hello-world/","2024/09/26/简单文件压缩器/","2024/10/05/截图工具/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
